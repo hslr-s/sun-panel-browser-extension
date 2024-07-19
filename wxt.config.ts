6c9a1975e2b3397dc69e795f62208eb533df6a3b
Sun <95302870@qq.com>
2024-07-19 22:11:58 +0800

    增加对应的参数依赖

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'wxt'
+import vueI18n from '@intlify/unplugin-vue-i18n/vite'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
+  vite: () => ({
+    plugins: [
+      // See https://vue-i18n.intlify.dev/guide/advanced/optimization.html
+      vueI18n({
+        include: 'assets/locales/*.json',
+      }),
+    ],
+  }),
   manifest: {
     permissions: ['storage', 'scripting', 'activeTab', 'contextMenus'],
     name: 'Sun-Panel BE',
