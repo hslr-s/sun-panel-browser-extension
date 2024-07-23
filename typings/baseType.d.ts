66e707742b4b3335c83176a95be5dd23963e44dc
Sun <95302870@qq.com>
2024-07-23 12:20:59 +0800

    删除无用的类型定义

---
+++ typings/baseType.d.ts
@@ -0,0 +1,12 @@
+declare namespace BaseType{
+  interface OpenAPIConfig {
+    host: string
+    token: string
+  }
+
+  interface HomePageConfig {
+    url: string
+    spareUrl: string
+    homePageInIframe: boolean
+  }
+}
