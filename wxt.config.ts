b2c3ca954bd6da85f55b8020db2a58fe0afb198a
sun <95302870@qq.com>
2025-01-23 14:08:48 +0800

    省去版本号字段，让编译自动根据version_name生成

--- wxt.config.ts
+++ wxt.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   manifest: {
     permissions: ['storage'],
     // name: 'Sun-Panel BE',
-    version: '0.1.4',
+    // version: '0.1.4',
     version_name: '0.1.4-beta',
     host_permissions: ['https://*/*', 'http://*/*', '*://*/*'],
     name: '__MSG_extName__',
